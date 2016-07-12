be97f8f8820b72dcb1ccfcf91317333c53c4547c
Greg Michalec <gmichalec@pandora.com>
2016-07-12 16:43:17 -0700

    rebuild

--- dist/select-tpls.js
+++ dist/select-tpls.js
@@ -95,6 +95,7 @@ angular.module('oi.select')
             if (event && event.target.nodeName !== 'INPUT') return; //for IE
 
             isBlur = false;
+            isFocused = false;
 
             if (isMousedown) {
                 isBlur = true;
@@ -921,7 +922,7 @@ angular.module('oi.select')
                 }
 
                 function compact(value) {
-                    value = value instanceof Array ? value : value ? [value]: [];
+                    value = value instanceof Array ? value : value !== undefined ? [value]: [];
 
                     return value.filter(function(item) {
                         return item !== undefined && (item instanceof Array && item.length || selectAsFn || getLabel(item));
